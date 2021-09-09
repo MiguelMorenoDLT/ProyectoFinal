5cf79564e23db75d5d5754124581ac85b4039e53
Cristian Duarte <duartec688@gmail.com>
2021-09-09 13:52:41 -0500

    Ajuste de Solicitar carga V1.0

--- app/src/main/java/co/edu/unipiloto/trucktrip/Loadout.kt
+++ app/src/main/java/co/edu/unipiloto/trucktrip/Loadout.kt
@@ -1,12 +1,22 @@
 package co.edu.unipiloto.trucktrip
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_loadout.*
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_mostrar_cargas.*
 import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.activity_registration.cerrarButton
 
 class Loadout : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loadout)
@@ -20,6 +30,38 @@ class Loadout : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
+
+
+        loadoutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString()).addOnCompleteListener{
+                if (it.isSuccessful){
+                        cargas(it.result?.user?.email ?: "")
+                }else{
+                    showAlert()
+                }
+            }
+          }
+        }
+
+    private fun cargas (email: String){
+
+        db.collection("users").document(email).get().addOnSuccessListener { basedatos ->
+            if (basedatos.exists()) {
+                val cargo: String? = basedatos.getString("Cargas")
+                Mostrar_Cargas.text = cargo
+            }
+        }
+        val intent = Intent(this, mostrar_cargas::class.java)
+        startActivity(intent)
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("No se han encontrado cargas")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
 }
---
+++ app/src/main/java/co/edu/unipiloto/trucktrip/mostrar_cargas.kt
@@ -0,0 +1,12 @@
+package co.edu.unipiloto.trucktrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_mostrar_cargas.*
+
+class mostrar_cargas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_cargas)
+    }
+}
