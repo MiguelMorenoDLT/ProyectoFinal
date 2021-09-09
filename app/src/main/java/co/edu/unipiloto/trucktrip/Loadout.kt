41fba9f5a7395c9621ef11f1ed4f2d66c4de329c
Cristian Duarte <duartec688@gmail.com>
2021-09-09 16:50:06 -0500

    Ajuste de Solicitar carga V1.1

--- app/src/main/java/co/edu/unipiloto/trucktrip/Loadout.kt
+++ app/src/main/java/co/edu/unipiloto/trucktrip/Loadout.kt
@@ -15,7 +15,7 @@ import kotlinx.android.synthetic.main.activity_registration.cerrarButton
 
 class Loadout : AppCompatActivity() {
 
-    val db = FirebaseFirestore.getInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,37 +31,9 @@ class Loadout : AppCompatActivity() {
             onBackPressed()
         }
 
-
         loadoutButton.setOnClickListener{
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString()).addOnCompleteListener{
-                if (it.isSuccessful){
-                        cargas(it.result?.user?.email ?: "")
-                }else{
-                    showAlert()
-                }
-            }
+            val intent = Intent(this, mostrar_cargas::class.java)
+            startActivity(intent)
           }
         }
-
-    private fun cargas (email: String){
-
-        db.collection("users").document(email).get().addOnSuccessListener { basedatos ->
-            if (basedatos.exists()) {
-                val cargo: String? = basedatos.getString("Cargas")
-                Mostrar_Cargas.text = cargo
-            }
-        }
-        val intent = Intent(this, mostrar_cargas::class.java)
-        startActivity(intent)
-    }
-
-    private fun showAlert() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage("No se han encontrado cargas")
-        builder.setPositiveButton("Aceptar", null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }
-
 }
--- app/src/main/java/co/edu/unipiloto/trucktrip/mostrar_cargas.kt
+++ app/src/main/java/co/edu/unipiloto/trucktrip/mostrar_cargas.kt
@@ -2,11 +2,45 @@ package co.edu.unipiloto.trucktrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_mostrar_cargas.*
 
 class mostrar_cargas : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_cargas)
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString()).addOnCompleteListener{
+            if (it.isSuccessful){
+                show_cargas(it.result?.user?.email ?: "")
+            }else{
+                showAlert()
+            }
+        }
     }
+    private fun show_cargas (email: String){
+
+        db.collection("users").document(email).get().addOnSuccessListener { basedatos ->
+            if (basedatos.exists()) {
+                val cargo: String? = basedatos.getString("Cargas")
+                Mostrar_Cargas.text = cargo
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("No se han encontrado cargas")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }
