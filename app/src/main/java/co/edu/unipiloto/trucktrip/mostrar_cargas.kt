0dfef52e4b18c898a6d50996bbb7aac72449c820
Cristian Duarte <duartec688@gmail.com>
2021-09-11 21:06:42 -0500

    Ajuste de solicitar carga v1.0

--- app/src/main/java/co/edu/unipiloto/trucktrip/mostrar_cargas.kt
+++ app/src/main/java/co/edu/unipiloto/trucktrip/mostrar_cargas.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_mostrar_cargas.*
-import kotlinx.android.synthetic.main.activity_registration.*
 
 class mostrar_cargas : AppCompatActivity() {
 
@@ -17,34 +15,21 @@ class mostrar_cargas : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_cargas)
 
-        /*val user = Firebase.auth.currentUser
-        user?.let {
-             val email = user.email
-             show_cargas(email.toString())
-        }*/
         getAllDocuments()
-
-    }
-    private fun show_cargas (email: String){
-
-        db.collection("Cargas").document(email).get().addOnSuccessListener { basedatos ->
-            if (basedatos.exists()) {
-                val cargo: String? = basedatos.getString("Cargas")
-                Mostrar_Cargas.text = cargo
-            }
-        }
     }
 
     private fun getAllDocuments (){
-        val spinner = findViewById<Spinner>(R.id.idSpinner)
-        var lista = listOf("")
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val arr = ArrayList<String>()
 
         db.collection("Cargas").get().addOnSuccessListener { basedatos ->
-           for (documento in basedatos ){
-               //Mostrar_Cargas.text = documento.get("Name_Load").toString()
-               lista = listOf(documento.get("Name_Load").toString())
-           }
-            val adaptor = ArrayAdapter(this, android.R.layout.simple_spinner_item,lista)
+            for (documento in basedatos ){
+                arr.add(documento.get("Name_Load").toString())
+            }
+            arr.forEach{
+                println(it)
+            }
+            val adaptor = ArrayAdapter(this, android.R.layout.simple_spinner_item, arr)
             spinner.adapter = adaptor
         }
     }
